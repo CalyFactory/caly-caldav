adbf4152972cf4b24f6273bea40475296e61b9ae
JSpiner <jspiner@naver.com>
2017-01-31 15:00:59 +0900

    add homeset

--- test.py
+++ test.py
@@ -1,1 +1,31 @@
 from caldavclient import CaldavClient
+import json 
+import time 
+from caldavclient import util
+
+with open('key.json') as json_data:
+    d = json.load(json_data)
+    userId = d['naver']['id']
+    userPw = d['naver']['pw']
+
+#hostname = "https://caldav.icloud.com"
+hostname = "https://caldav.calendar.naver.com/principals/users/jspiner"
+
+## 기본 client 생성
+client = CaldavClient(
+    hostname,
+    userId,
+    userPw
+)
+
+principal = client.getPrincipal()
+homeset = principal.getHomeSet()
+
+calendars = homeset.getCalendars()
+
+for calendar in calendars:
+    print(calendar.calendarName + " " + calendar.calendarUrl + " " + calendar.cTag)
+
+eventList = calendars[0].getAllEvent()
+for event in eventList:
+    print (event.eTag)
