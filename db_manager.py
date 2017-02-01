d24b79e215462538d8254a2096bc175b7e23df91
refgjin <refgjin@gmail.com>
2017-02-01 15:32:22 +0900

    Extract event data from .ics : in modify

--- db_manager.py
+++ db_manager.py
@@ -9,10 +9,12 @@ def extractEvent(homeset_cal_id, cal_id, ics_list):
 	evt_list=[]
 	path=os.getcwd()+"/" # Declare for generate ics file path
 	# Get auth
-	res = db_connector.query("select user_base64 from account where host_name=%s",(homeset_cal_id,))
+	res = db_connector.query("select user_base64,user_id,host_name from account where host_name=%s",(homeset_cal_id,))
 	rows = util.fetch_all_json(res)
 	if rows[0]['user_base64'] is None:
 		return #handle error message
+	user_id = rows[0]['user_id']
+	host_name = rows[0]['host_name']
 	Auth="Basic "+rows[0]['user_base64']
 	# loop 
 	""" """
@@ -20,9 +22,24 @@ def extractEvent(homeset_cal_id, cal_id, ics_list):
 		# file open
 		url_resp = requests.request("GET",homeset_cal_id+cal_id+ics,headers={"Depth":"1","Authorization":Auth})
 		ics_data = str(url_resp.text)
-		#print(ics_data.findall('SUM'))
-		#print(ics_data.find("SUMMARY:"))
-		#print(ics_data)
+		
+		#print("EVT_NAME :",ics_data[ics_data.find("SUMMARY:")+8:ics_data.find("DTSTART;")])
+		evt_name = ics_data[ics_data.find("SUMMARY:")+8:ics_data.find("DTSTART;")]
+		#print("EVT_START_TIME :",ics_data[ics_data.find("DTSTART;")+8:ics_data.find("DTSTAMP:")])
+		evt_start_dt = ics_data[ics_data.find("DTSTART;")+8:ics_data.find("DTSTAMP:")]
+		#print("EVT_END_TIME :",ics_data[ics_data.find("DTEND;")+6:ics_data.find("SUMMARY:")])
+		evt_end_dt = ics_data[ics_data.find("DTEND;")+6:ics_data.find("SUMMARY:")]
+		evt_loc = None
+		tmp_ics_data = ics_data.replace("-LOCATION","") # Except X-LIC-LOCATION id column
+		if tmp_ics_data.find("LOCATION:") is not -1:
+			#print("EVT_LOCATION :",tmp_ics_data[tmp_ics_data.find("LOCATION:")+9:tmp_ics_data.find("SEQUENCE:")])
+			evt_loc = tmp_ics_data[tmp_ics_data.find("LOCATION:")+9:tmp_ics_data.find("SEQUENCE:")]
+		
+		if evt_loc is not None:
+			evt_query = db_connector.query("UPDATE event SET event_name=%s,location=%s,start_dt=%s,end_dt=%s WHERE host_name=%s,user_id=%s,calendar_id=%s,event_id=%s",(evt_name, evt_loc, evt_start_dt, evt_end_dt, host_name, user_id, cal_id, ics))
+		else:
+			evt_query = db_connector.query("UPDATE event SET event_name=%s,location=null,start_dt=%s,end_dt=%s WHERE host_name=%s,user_id=%s,calendar_id=%s,event_id=%s",(evt_name, evt_start_dt, evt_end_dt, host_name, user_id, cal_id, ics))
+	#print("WHERE")
 		# generate data (event name, dt:start~end, location)
 		
 		# remove file
@@ -50,7 +67,7 @@ def initInsertCalendars(client, principal, homeset, calendar_list):
 
 		for evt in evt_list:
 			result3 = db_connector.query("INSERT INTO event (host_name, user_id, calendar_id, event_id, e_tag) VALUES (%s, %s, %s, %s, %s)",(client.hostname, client.auth[0], calendar.calendarId, evt.eventId, evt.eTag))
-"""	
+"""	"""
 
 homeset_cal_id="https://p58-caldav.icloud.com/10836055664/calendars/"
 cal_id="home/"
@@ -59,4 +76,3 @@ ics_ls=[]
 ics_ls.append("159EBF61-05FB-457F-BAE2-5C4A2EE85322.ics")
 ics_ls.append("80D1290F-1647-48E4-A85B-A5746188BA80.ics")
 extractEvent(homeset_cal_id, cal_id,ics_ls)
-"""
